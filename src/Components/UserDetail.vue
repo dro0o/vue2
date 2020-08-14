53431e66c16c288903455f6d966ab0d9387487f4
dro0o <andrew@pittman.dev>
2020-08-14 08:29:35 -0400

    event bus logic

--- src/Components/UserDetail.vue
+++ src/Components/UserDetail.vue
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+	import { eventBus } from '../main'
 	export default {
 		props: {
 			name: {
@@ -31,6 +32,11 @@
 				this.$emit('nameWasReset', this.name)
 			}
 		},
+		created() {
+			eventBus.$on('ageWasEdited', (age) => {
+				this.userAge = age
+			})
+		}
 	}
 </script>
 
