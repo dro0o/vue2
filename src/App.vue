9eaff9006a5a1ab17deab9ae88ba5a6191123c48
dro0o <andrew@pittman.dev>
2020-08-14 15:48:36 -0400

    ex 8 solution

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<button @click="selectedComponent = 'appQuote'">Quote</button>
+				<!-- <button @click="selectedComponent = 'appQuote'">Quote</button>
 				<button @click="selectedComponent = 'appAuthor'">Author</button>
 				<button @click="selectedComponent = 'appNew'">New</button>
 				<hr />
@@ -11,11 +11,21 @@
 					<component :is="selectedComponent">
 						<p>Default Content</p>
 					</component>
-				</keep-alive>
+				</keep-alive>-->
 				<!-- <app-quote>
 					<h2 slot="title">{{quoteTitle}}</h2>
 					<p>A wonderful quote</p>
 				</app-quote>-->
+				<br />
+				<button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+				<button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+				<button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+				<hr />
+				<keep-alive>
+					<component :is="selectedComponent">
+						<h2>Well hello there...</h2>
+					</component>
+				</keep-alive>
 			</div>
 		</div>
 	</div>
@@ -25,16 +35,22 @@
 	import Quote from './components/Quote'
 	import Author from './components/Author'
 	import New from './components/New'
+	import Blue from './components/Blue.vue'
+	import Green from './components/Green.vue'
+	import Red from './components/Red.vue'
 	export default {
 		components: {
 			appQuote: Quote,
 			appAuthor: Author,
-			appNew: New
+			appNew: New,
+			appBlue: Blue,
+			appGreen: Green,
+			appRed: Red
 		},
 		data() {
 			return {
 				quoteTitle: 'The Quote',
-				selectedComponent: 'appQuote'
+				selectedComponent: 'appBlue'
 			}
 		}
 	}
@@ -42,6 +58,6 @@
 
 <style>
 	h2 {
-		color: red;
+		color: teal;
 	}
 </style>
