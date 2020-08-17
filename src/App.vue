ef305ccd73cde6dd03bd96496586b0b63012bd3e
dro0o <andrew@pittman.dev>
2020-08-17 08:29:27 -0400

    baseline quote functionality

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-	<div class="container"></div>
+	<div class="container">
+		<app-quote-grid :quotes="quotes"></app-quote-grid>
+	</div>
 </template>
 
 <script>
+	import QuoteGrid from './components/QuoteGrid'
 	export default {
-
+		data() {
+			return {
+				maxQuotes: 10,
+				quotes: [
+					'Just a Quote to see something wonderful!'
+				]
+			}
+		},
+		components: {
+			appQuoteGrid: QuoteGrid
+		}
 	}
 </script>
 
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="row">
+		<app-quote v-for="(quote, i) in quotes" :key="i">{{ quote }}</app-quote>
+	</div>
+</template>
+
+<script>
+	import Quote from './Quote'
+	export default {
+		props: ['quotes'],
+		components: {
+			appQuote: Quote
+		}
+	}
+</script>
+
+<style scoped></style>
