adefff8edfccfb9fb8240d087b5c7ed31f7a3d93
dro0o <andrew@pittman.dev>
2020-08-18 11:52:21 -0400

    multiple modifiers, object props on custom directive

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,9 @@
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 				<h1>Custom Directives</h1>
 				<p v-highlight:background.delayed="'red'">Color this!</p>
-				<p v-local-highlight:background.delayed="'red'">Color this, locally!</p>
+				<p
+					v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}"
+				>Color this, locally!</p>
 			</div>
 		</div>
 	</div>
@@ -27,13 +29,29 @@
 					if (binding.modifiers["delayed"]) {
 						delay = 3000
 					}
-					setTimeout(() => {
-						if (binding.arg == "background") {
-							el.style.backgroundColor = binding.value
-						} else {
-							el.style.color = binding.value
-						}
-					}, delay)
+					if (binding.modifiers["blink"]) {
+						let mainColor = binding.value.mainColor
+						let secondColor = binding.value.secondColor
+						let currentColor = mainColor
+						setTimeout(() => {
+							setInterval(() => {
+								currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor
+								if (binding.arg == "background") {
+									el.style.backgroundColor = currentColor
+								} else {
+									el.style.color = currentColor
+								}
+							}, 1000)
+						}, binding.value.delay)
+					} else {
+						setTimeout(() => {
+							if (binding.arg == "background") {
+								el.style.backgroundColor = binding.value.mainColor
+							} else {
+								el.style.color = binding.value.mainColor
+							}
+						}, delay)
+					}
 				}
 			}
 		}
