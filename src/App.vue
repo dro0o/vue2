b6226c5d2d6a1e40f5bb468e89567d163dcd68b9
dro0o <andrew@pittman.dev>
2020-08-18 11:32:30 -0400

    custom directives ,w/ props

--- src/App.vue
+++ src/App.vue
@@ -1,104 +1,22 @@
 <template>
 	<div class="container">
-		<!-- Exercise 1 -->
-		<!-- Create a Signup Form where you retrieve the following Information -->
-		<!-- Full Name (First Name + Last Name) -->
-		<!-- Mail -->
-		<!-- Password -->
-		<!-- Store Data? Yes/No -->
-
-		<!-- Exercise 2 -->
-		<!-- Only display the Form if it has NOT been submitted -->
-		<!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-		<!-- Exercise 3 -->
-		<!-- Edit the Example from above and create a custom "Full Name" Control -->
-		<!-- which still holds the First Name and Last Name Input Field -->
-		<form>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-					<h1>Practice 11</h1>
-					<div class="form-group">
-						<app-full-name v-model="name"></app-full-name>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-					<div class="form-group">
-						<label for="mail">Mail</label>
-						<input type="text" id="mail" class="form-control" v-model.lazy="mail" />
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-					<div class="form-group">
-						<label for="password">Password</label>
-						<input type="password" id="password" class="form-control" v-model="password" />
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-					<label>Store Data?</label>
-					<label for="Yes">
-						<input type="radio" id="Yes" value="Yes" v-model="storeData" /> Yes
-					</label>
-					<label for="No">
-						<input type="radio" id="No" value="No" v-model="storeData" /> No
-					</label>
-				</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<h1>Directives</h1>
+				<p v-text="'Some Text'"></p>
+				<p v-html="'<strong>blah</blah>'"></p>
 			</div>
 			<hr />
-			<div class="row">
-				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-					<button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
-				</div>
-			</div>
-		</form>
-		<hr />
-		<div class="row" v-if="isSubmitted">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4>Your Data</h4>
-					</div>
-					<div class="panel-body">
-						<p>Full Name: {{name.first}} {{name.last}}</p>
-						<p>Mail: {{mail}}</p>
-						<p>Password: {{password}}</p>
-						<p>Store in Database?: {{storeData}}</p>
-					</div>
-				</div>
+				<h1>Custom Directives</h1>
+				<p v-highlight:background="'red'">Color this!</p>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-	import FullName from './FullName'
 	export default {
-		data() {
-			return {
-				name: {
-					first: '',
-					last: ''
-				},
-				mail: '',
-				password: '',
-				storeData: 'Yes',
-				isSubmitted: false
-			}
-		},
-		methods: {
-			submitted() {
-				this.isSubmitted = true
-			}
-		},
-		components: {
-			appFullName: FullName
-		}
 	}
 </script>
 
