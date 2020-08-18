577672005c956fc7a432716a618053972115e91b
dro0o <andrew@pittman.dev>
2020-08-18 12:59:15 -0400

    local filter usage

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<h1>Directives</h1>
-				<p v-text="'Some Text'"></p>
-				<p v-html="'<strong>blah</blah>'"></p>
-			</div>
-			<hr />
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<h1>Custom Directives</h1>
-				<p v-highlight:background.delayed="'red'">Color this!</p>
-				<p
-					v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}"
-				>Color this, locally!</p>
-			</div>
-		</div>
-		<hr />
-		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<h1>Directives Exercise</h1>
-				<!-- Exercise -->
-				<!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-				<button v-myon:click="testFunc" class="btn btn-primary">Run testFunc!</button>
-				<hr />
-				<div
-					style="width: 100px; height: 100px; background-color: lightgreen"
-					v-myon:mouseenter="mouseEnter"
-					v-myon:mouseleave="mouseLeave"
-				></div>
+				<h1>Filters & Mixins</h1>
+				<p>{{text | toUppercase}}</p>
 			</div>
 		</div>
 	</div>
@@ -35,60 +11,14 @@
 
 <script>
 	export default {
-		directives: {
-			'local-highlight': {
-				bind(el, binding, vnode) {
-					// el.style.backgroundColor = "green"
-					// el.style.backgroundColor = binding.value
-					var delay = 0
-					if (binding.modifiers["delayed"]) {
-						delay = 3000
-					}
-					if (binding.modifiers["blink"]) {
-						let mainColor = binding.value.mainColor
-						let secondColor = binding.value.secondColor
-						let currentColor = mainColor
-						setTimeout(() => {
-							setInterval(() => {
-								currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor
-								if (binding.arg == "background") {
-									el.style.backgroundColor = currentColor
-								} else {
-									el.style.color = currentColor
-								}
-							}, 1000)
-						}, binding.value.delay)
-					} else {
-						setTimeout(() => {
-							if (binding.arg == "background") {
-								el.style.backgroundColor = binding.value.mainColor
-							} else {
-								el.style.color = binding.value.mainColor
-							}
-						}, delay)
-					}
-				}
-			},
-			'myon': {
-				bind(el, binding, vnode) {
-					// el.onclick = () => {
-					// 	binding.value()
-					// }
-					const type = binding.arg
-					const fn = binding.value
-					el.addEventListener(type, fn)
-				}
+		data() {
+			return {
+				text: 'Hello there!'
 			}
 		},
-		methods: {
-			testFunc() {
-				alert('you have run testFunc!')
-			},
-			mouseEnter() {
-				console.log('mouse entered!')
-			},
-			mouseLeave() {
-				console.log('mouse leave!')
+		filters: { //local filters
+			toUppercase(value) { // same as custom components
+				return value.toUpperCase()
 			}
 		}
 	}
