d4e7db23b132f27a38d54fa54d2b852c95b279b3
dro0o <andrew@pittman.dev>
2020-08-18 15:22:12 -0400

    transition functioning

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 				<h1>Animations</h1>
+				<hr />
+				<button class="btn btn-primary" @click="show = !show">Show Alert</button>
+				<br />
+				<br />
+				<transition name="fade">
+					<div class="alert alert-info" v-if="show">This is some info</div>
+				</transition>
 			</div>
 		</div>
 	</div>
@@ -12,11 +19,24 @@
 	export default {
 		data() {
 			return {
-
+				show: false
 			}
 		}
 	}
 </script>
 
 <style>
+	.fade-enter {
+		opacity: 0;
+	}
+	.fade-enter-active {
+		transition: opacity 1s;
+	}
+	.fade-leave {
+		/* opacity: 1; */
+	}
+	.fade-leave-active {
+		transition: opacity 1s;
+		opacity: 0;
+	}
 </style>
