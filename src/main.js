e4090a0aa8149e7b138a8b2584b359fbd4a48184
dro0o <andrew@pittman.dev>
2020-08-18 13:06:21 -0400

    computed data instead of filters, better perf

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue"
 import App from "./App.vue"
 
-Vue.filter("to-lowercase")
+Vue.filter("to-lowercase", (value) => {
+  return value.toLowerCase()
+})
 
 new Vue({
   el: "#app",
