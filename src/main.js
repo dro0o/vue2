ad492a72863b4c764164c6e87cfc6ac5de65950d
dro0o <andrew@pittman.dev>
2020-08-19 14:18:56 -0400

    set up routing

--- src/main.js
+++ src/main.js
@@ -1,23 +1,5 @@
 import Vue from "vue"
-import VueResource from "vue-resource"
 import App from "./App.vue"
-
-Vue.use(VueResource)
-
-// no $ when accessing global from main.js
-// PUT replaces data, POST appends data in FB
-Vue.http.options.root = "https://vuejs-http-e994d.firebaseio.com/"
-Vue.http.interceptors.push((request, next) => {
-  console.log(request)
-  if (request.method == "POST") {
-    request.method = "PUT"
-  }
-  next((response) => {
-    response.json = () => {
-      return { messages: response.body }
-    }
-  })
-})
 
 new Vue({
   el: "#app",
