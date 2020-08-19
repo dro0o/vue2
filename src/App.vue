67a07c9eab96215edd4d23cc244f448efc5b6de0
dro0o <andrew@pittman.dev>
2020-08-19 10:14:34 -0400

    set up math question app

--- src/App.vue
+++ src/App.vue
@@ -2,208 +2,41 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<h1>Animations</h1>
-				<hr />
-				<select v-model="alertAnimation" class="form-control">
-					<option value="fade">Fade</option>
-					<option value="slide">Slide</option>
-				</select>
-				<br />
-				<br />
-				<button class="btn btn-primary" @click="show = !show">Show Alert</button>
-				<br />
-				<br />
-				<transition :name="alertAnimation">
-					<div class="alert alert-info" v-show="show">This is some info</div>
-				</transition>
-				<br />
-				<transition appear name="slide" type="animation">
-					<div class="alert alert-info" v-if="show">This is some info</div>
-				</transition>
-				<br />
-				<transition
-					appear
-					enter-active-class="animate__animated animate__bounce"
-					leave-active-class="animate__animated animate__shakeX"
-				>
-					<div class="alert alert-info" v-if="show">This is some info</div>
-				</transition>
-				<br />
-				<br />
-				<transition :name="alertAnimation" mode="out-in">
-					<div class="alert alert-info" v-if="show" key="info">This is some info</div>
-					<div class="alert alert-warning" v-else key="warning">This is some info</div>
-				</transition>
-				<hr />
-				<button class="btn btn-primary" @click="load=!load">Load/Remove Element</button>
-				<br />
-				<br />
-				<transition
-					@before-enter="beforeEnter"
-					@enter="enter"
-					@after-enter="afterEnter"
-					@enter-cancelled="enterCancelled"
-					@before-leave="beforeLeave"
-					@leave="leave"
-					@after-leave="afterLeave"
-					@leave-cancelled="leaveCancelled"
-					:css="false"
-				>
-					<div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
-				</transition>
-				<hr />
-				<button
-					class="btn btn-primary"
-					@click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'"
-				>Switch dynamic component</button>
-				<hr />
-				<transition name="fade" mode="out-in">
-					<component :is="selectedComponent"></component>
-				</transition>
-				<hr />
-				<button class="btn btn-primary" @click="addItem">Add Item</button>
-				<hr />
-				<ul class="list-group">
-					<transition-group name="slide">
-						<li
-							class="list-group-item"
-							v-for="(number, ind) in numbers"
-							@click="removeItem(ind)"
-							style="cursor: pointer"
-							:key="number"
-						>{{ number }}</li>
-					</transition-group>
-				</ul>
+				<h1 class="text-center">The Super Quiz</h1>
+			</div>
+		</div>
+		<hr />
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-	import DangerAlert from './DangerAlert'
-	import SuccessAlert from './SuccessAlert'
+	import Question from './components/Question.vue'
+	import Answer from './components/Answer.vue'
+
 	export default {
 		data() {
 			return {
-				show: false,
-				alertAnimation: 'fade',
-				load: true,
-				elementWidth: 100,
-				selectedComponent: 'appDangerAlert',
-				numbers: [1, 2, 3, 4, 5]
+				mode: 'app-question'
 			}
 		},
 		methods: {
-			beforeEnter(el) {
-				console.log('beforeEnter')
-				this.elementWidth = 100
-				el.style.width = this.elementWidth + 'px'
-			},
-			enter(el, done) {
-				console.log('enter')
-				let round = 1
-				const interval = setInterval(() => {
-					el.style.width = (this.elementWidth + round * 10) + 'px'
-					round++
-					if (round > 20) {
-						clearInterval(interval)
-						done()
-					}
-				}, 20)
-			},
-			afterEnter(el) {
-				console.log('afterEnter')
-			},
-			enterCancelled(el) {
-				console.log('enterCancelled')
-			},
-			beforeLeave(el) {
-				console.log('beforeLeave')
-				this.elementWidth = 300
-				el.style.width = this.elementWidth + 'px'
-			},
-			leave(el, done) {
-				console.log('leave')
-				let round = 1
-				const interval = setInterval(() => {
-					el.style.width = (this.elementWidth - round * 10) + 'px'
-					round++
-					if (round > 20) {
-						clearInterval(interval)
-						done()
-					}
-				}, 20)
-			},
-			afterLeave(el) {
-				console.log('afterLeave')
-			},
-			leaveCancelled(el) {
-				console.log('leaveCancelled')
-			},
-			addItem() {
-				const pos = Math.floor(Math.random() * this.numbers.length)
-				this.numbers.splice(pos, 0, this.numbers.length + 1)
-			},
-			removeItem(ind) {
-				this.numbers.splice(ind, 1)
+			answered(isCorrect) {
+				if (isCorrect) {
+					this.mode = 'app-answer'
+				} else {
+					this.mode = 'app-question'
+					alert('Wrong, try again!')
+				}
 			}
 		},
 		components: {
-			appDangerAlert: DangerAlert,
-			appSuccessAlert: SuccessAlert
+			appQuestion: Question,
+			appAnswer: Answer
 		}
 	}
 </script>
-
-<style>
-	.fade-enter {
-		opacity: 0;
-	}
-	.fade-enter-active {
-		transition: opacity 1s;
-	}
-	.fade-leave {
-		/* opacity: 1; */
-	}
-	.fade-leave-active {
-		transition: opacity 1s;
-		opacity: 0;
-	}
-	.slide-enter {
-		opacity: 0;
-		/* transform: translate(20px); */
-	}
-	.slide-enter-active {
-		animation: slide-in 1s ease-out forwards;
-		transition: opacity 0.5s;
-	}
-	.slide-leave {
-	}
-	.slide-leave-active {
-		animation: slide-out 1s ease-out forwards;
-		transition: opacity 1s;
-		opacity: 0;
-		position: absolute;
-	}
-
-	.slide-move {
-		transition: transform 1s;
-	}
-
-	@keyframes slide-in {
-		from {
-			transform: translateY(20px);
-		}
-		to {
-			transform: translateY(0);
-		}
-	}
-	@keyframes slide-out {
-		from {
-			transform: translateY(0);
-		}
-		to {
-			transform: translateY(20px);
-		}
-	}
-</style>
