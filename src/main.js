f73793d14f8958a2e0bf5c7a3d86696cce22171f
dro0o <andrew@pittman.dev>
2020-08-19 13:52:58 -0400

    intercept requests and responses

--- src/main.js
+++ src/main.js
@@ -5,7 +5,19 @@ import App from "./App.vue"
 Vue.use(VueResource)
 
 // no $ when accessing global from main.js
+// PUT replaces data, POST appends data in FB
 Vue.http.options.root = "https://vuejs-http-e994d.firebaseio.com/data.json"
+Vue.http.interceptors.push((request, next) => {
+  console.log(request)
+  if (request.method == "POST") {
+    request.method = "PUT"
+  }
+  next((response) => {
+    response.json = () => {
+      return { messages: response.body }
+    }
+  })
+})
 
 new Vue({
   el: "#app",
