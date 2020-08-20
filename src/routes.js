b138673f9c2990081f4e63d2950d64b433f0f35d
dro0o <andrew@pittman.dev>
2020-08-20 12:01:14 -0400

    use another param to bundle lazy loaded components

--- src/routes.js
+++ src/routes.js
@@ -3,27 +3,43 @@ import Header from "./components/Header.vue"
 
 // Lazy loading of the User page, use with webpack
 const User = (resolve) => {
-  require.ensure(["./components/user/User.vue"], () => {
-    resolve(require("./components/user/User.vue"))
-  })
+  require.ensure(
+    ["./components/user/User.vue"],
+    () => {
+      resolve(require("./components/user/User.vue"))
+    },
+    "user"
+  )
 }
 
 const UserEdit = (resolve) => {
-  require.ensure(["./components/user/UserEdit.vue"], () => {
-    resolve(require("./components/user/UserEdit.vue"))
-  })
+  require.ensure(
+    ["./components/user/UserEdit.vue"],
+    () => {
+      resolve(require("./components/user/UserEdit.vue"))
+    },
+    "user"
+  )
 }
 
 const UserDetail = (resolve) => {
-  require.ensure(["./components/user/UserDetail.vue"], () => {
-    resolve(require("./components/user/UserDetail.vue"))
-  })
+  require.ensure(
+    ["./components/user/UserDetail.vue"],
+    () => {
+      resolve(require("./components/user/UserDetail.vue"))
+    },
+    "user"
+  )
 }
 
 const UserStart = (resolve) => {
-  require.ensure(["./components/user/UserStart.vue"], () => {
-    resolve(require("./components/user/UserStart.vue"))
-  })
+  require.ensure(
+    ["./components/user/UserStart.vue"],
+    () => {
+      resolve(require("./components/user/UserStart.vue"))
+    },
+    "user"
+  )
 }
 
 export const routes = [
