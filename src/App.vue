a99b6a80e0fc0abc52669140d205f33883e2439e
dro0o <andrew@pittman.dev>
2020-08-20 15:58:05 -0400

    two way binding to vuex store with computed get and set methods

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         <hr />
         <app-another-counter></app-another-counter>
         <hr />
-        <input type="text" :value="value" @input="updateValue" />
+        <input type="text" v-model="value" />
         <p>{{ value }}</p>
       </div>
     </div>
@@ -25,8 +25,13 @@ import AnotherResult from "./components/AnotherResult.vue"
 
 export default {
   computed: {
-    value() {
-      return this.$store.getters.value
+    value: {
+      get() {
+        return this.$store.getters.value
+      },
+      set(value) {
+        this.$store.dispatch("updateValue", value)
+      },
     },
   },
   methods: {
