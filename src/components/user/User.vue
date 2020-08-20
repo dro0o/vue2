50b3b103c206babb91e73b458ae8bde8ebbd06a5
dro0o <andrew@pittman.dev>
2020-08-20 10:30:18 -0400

    functioning children routing with params

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,28 +1,19 @@
 <template>
-  <div>
-    <h1>The User Page</h1>
-    <hr />
-    <p>Loaded ID: {{ id }}</p>
-    <button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
-  </div>
+	<div>
+		<h1>The User Page</h1>
+		<hr />
+		<button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
+		<hr />
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-    }
-  },
-  watch: {
-    $route(to, from) {
-      this.id = to.params.id
-    },
-  },
-  methods: {
-    navigateToHome() {
-      this.$router.push({ path: "/" })
-    },
-  },
-}
+	export default {
+		methods: {
+			navigateToHome() {
+				this.$router.push({ path: "/" })
+			},
+		},
+	}
 </script>
