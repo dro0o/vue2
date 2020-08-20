97b6ccd0ddd4888dedb3725ddeaed58bb42f76a3
dro0o <andrew@pittman.dev>
2020-08-20 14:52:16 -0400

    working vuex state, mapGetters for easy getter usage

---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+export default {
+  // can use object to rename mapGetters assignement to local var
+  // computed: mapGetters({counter: 'doubleCounter'})
+  computed: {
+    ...mapGetters(["doubleCounter", "stringCounter"]),
+  }, // use spread to make local computed properties
+}
+</script>
